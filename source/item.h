80bde3d66e6cab3ce99d2e50ead6f0a8ebb78aae
erikj23 <erikj23.em@gmail.com>
2018-05-14 16:54:33 -0700

    Implemented item.h

--- source/item.h
+++ source/item.h
@@ -1,1 +1,25 @@
 
+#include <string>
+
+class Item
+{
+private:
+  double fullPrice{0.0};
+
+protected:
+  // unique item identification number
+  int itemID{0};
+  
+  // type of item
+  std::string itemType{""};
+  
+  // initialize in source
+  static int totalItems; 
+
+public:
+
+  std::string getItemType() const;
+  std::string getItem() const;
+  double getFullPrice() const;
+
+};
