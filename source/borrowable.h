ebe220f9382024cc5f4f5acc2a1418830641cd51
TIEN HUYNH <tien.huynh5312@gmail.com>
2018-05-14 16:57:29 -0700

    Update borrowable.h

--- source/borrowable.h
+++ source/borrowable.h
@@ -1,1 +1,28 @@
+#ifndef _BORROWABLE_H
+#define _BORROWABLE_H
 
+class Borrowable
+{
+  private:
+  int itemNumber;
+  int totalSupply;
+  bool borrowable;
+  double borrowingPrice;
+
+  public:
+  Borrowable();
+  Borrowable(const int& total = 0, const double& price = 0.0);
+
+  ~Borrowable();
+
+  bool borrow(const int& quantity);
+  bool return(const int& quantity);
+
+  int& getCurrentStock() const;
+  int& getTotalStock() const;
+  bool isBorrowable() const;
+
+  double& getBorrowingPrice() const;
+}
+
+#endif
