e83340f85ea6c437e91c2b0b28d1624bad5d1949
tienhuynh5312 <tien.huynh5312@gmail.com>
2018-05-16 20:10:30 -0700

    Borrowable Implementation

---
+++ source/borrowable.cpp
@@ -0,0 +1,70 @@
+#include "borrowable.h"
+
+Borrowable::Borrowable() 
+{
+    itemBorrowed = 0;
+    totalSupply = 10;
+    borrowable = false;
+    borrowingPrice = 0.0;
+}
+
+Borrowable::Borrowable(const int &total = 0, const double &price = 0.0, const bool &borrow = true)
+    : borrowingPrice(price), borrowable(borrow)
+{
+    if (total < 0)
+        totalSupply = 0;
+    else
+        totalSupply = total;
+}
+
+Borrowable::~Borrowable()
+{
+}
+
+bool Borrowable::borrowItem(const int &quantity)
+{
+    if (borrowable == false)
+        return false;
+
+    if (quantity > getCurrentStock())
+        return false;
+    else
+    {
+        itemBorrowed += quantity;
+        return true;
+    }
+}
+bool Borrowable::returnItem(const int &quantity)
+{
+    if (quantity < 0)
+        return false;
+    else
+    {
+        itemBorrowed -= quantity;
+    }
+}
+
+int Borrowable::getCurrentStock() const
+{
+    return totalSupply - itemBorrowed;
+}
+
+int Borrowable::getTotalStock() const
+{
+    return totalSupply;
+}
+
+bool Borrowable::isBorrowable() const
+{
+    return borrowable;
+}
+
+double Borrowable::getBorrowingPrice() const
+{
+    return borrowingPrice;
+}
+
+bool Borrowable::setTotalSupply(const int& total)
+{
+    totalSupply = total;
+}
