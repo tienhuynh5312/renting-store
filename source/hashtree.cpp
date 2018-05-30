a705649030281c5657ae6c3b73b1db22d03210d3
tienhuynh5312 <tien.huynh5312@gmail.com>
2018-05-30 14:51:19 -0700

    Update HashTree

--- source/hashtree.cpp
+++ source/hashtree.cpp
@@ -1,30 +1,21 @@
 
-#ifndef HASH_TREE_CPP
-#define HASH_TREE_CPP
 
+#include <list>
 #include <memory>
-<<<<<<< HEAD
-#include "customer.h"
-=======
-#include <vector>
->>>>>>> 45f7885d4adcfd14bdf483edcd0a1b03f102a051
-
 #include "hashtree.h"
+#include "customer.h"
 
 template <class KeyType, class ItemType>
-HashTree<KeyType, ItemType>::HashTree() try : hashTable(new std::list<std::pair<KeyType, ItemType>>[ DEFAULT_CAPACITY ]), hashTableCapacity(DEFAULT_CAPACITY)
+HashTree<KeyType, ItemType>::HashTree()
 {
-}
-catch (std::exception &e)
-{ // alloc error thrown
+  hashTable = new std::list<std::pair<KeyType, ItemType>>[ DEFAULT_CAPACITY ];
+  hashTableCapacity = DEFAULT_CAPACITY;
 }
 
 template <class KeyType, class ItemType>
-HashTree<KeyType, ItemType>::HashTree(const std::size_t &capacity) try : hashTable(new std::list<std::pair<KeyType, ItemType>>[ capacity ]), hashTableCapacity(capacity)
-{
-}
-catch (std::exception &e)
-{ // alloc error thrown
+HashTree<KeyType, ItemType>::HashTree(const std::size_t &capacity){
+  hashTable = new std::list<std::pair<KeyType, ItemType>>[ capacity ];
+  hashTableCapacity = capacity;
 }
 
 template <class KeyType, class ItemType>
@@ -37,33 +28,22 @@ HashTree<KeyType, ItemType>::~HashTree()
 template <class KeyType, class ItemType>
 bool HashTree<KeyType, ItemType>::add(const KeyType &key, ItemType value)
 {
-<<<<<<< HEAD
 
   const int index = key % hashTableCapacity;
   if (contains(key) == nullptr)
   {
-    KeyType k = key;
-    ItemType v = nullptr;
-    std::pair<KeyType, ItemType> item = std::make_pair(k, v);
-    this->hashTable[index].push_back(std::move(std::make_pair(k, value)));
+    ItemType v{value};
+    this->hashTable[index].push_back(std::make_pair(key, v));
     this->hashTable[index].back().second = value;
     return true;
   }
   else
     return false;
-=======
-}
-
-template <typename Int, typename HashableType>
-bool HashTree<Int, HashableType>::remove(const HashableType &value)
-{
->>>>>>> 45f7885d4adcfd14bdf483edcd0a1b03f102a051
 }
 
 template <class KeyType, class ItemType>
 bool HashTree<KeyType, ItemType>::remove(const KeyType &key)
 {
-<<<<<<< HEAD
   const int index = key % hashTableCapacity;
   for (auto i = hashTable[index].begin(); i != hashTable[index].end(); i++)
   {
@@ -88,14 +68,7 @@ ItemType HashTree<KeyType, ItemType>::contains(const KeyType &key)
     }
   }
   return nullptr;
-=======
-}
-
-template <typename Int, typename HashableType>
-bool HashTree<Int, HashableType>::foreach(void visit(const HasableType &value))
-{ // begining of hashtree
->>>>>>> 45f7885d4adcfd14bdf483edcd0a1b03f102a051
 }
 
 template class HashTree<int, std::shared_ptr<Customer>>;
-#endif
+template class HashTree<int, Customer*>;
