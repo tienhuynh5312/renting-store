2ffc13508685f7e7b83f4ac87c2f298892e8fd63
erikj23 <erikj23.em@gmail.com>
2018-06-01 13:38:22 -0700

    Comments

--- source/classics.h
+++ source/classics.h
@@ -12,17 +12,78 @@ using namespace std;
 class Classics : public Video
 {
 public:
-    friend ostream &operator<<(ostream &outStream, const Classics &rhs );
+  
+  /**
+   * @brief Operator overload that returns an ostream object reference.
+   * 
+   * @param outStream ostream reference to be returned
+   * @param rhs Classics object to send to ostream
+   * 
+   * @return ostream containing Classics object data
+   */
+  friend ostream &operator<<(ostream &outStream, const Classics &rhs);
+  
+  /**
+   * @brief Construct a classics object with the videoType set to classic.
+   */
   Classics() { videoType = VideoType::CLASSICS; };
   virtual ~Classics();
 
+  /**
+   * @brief Construct a classics object with the given parameters.
+   * 
+   * @param title Title of classics video
+   * @param stock Initial stock of classics video
+   * @param director Director of classics video
+   */
   Classics(const std::string &title, int stock, const std::string &director);
-  Classics(const std::string &title, int stock, const std::string &director, const std::string &majorActor, const Date &date);
-  const std::string &getMajorActor() const;
-  bool operator<(const Classics &rhs) const;
-  bool operator==(const Classics &rhs) const;
-  int getHash() const;
 
+  /**
+   * @brief
+   * 
+   * @param title Title if classics video
+   * @param stock Initial stock of classics video
+   * @param director Director of classics video
+   * @param majorActor Major actor for classics video
+   * @param date Date of classics video
+   */
+  Classics(const std::string &title, int stock, const std::string &director, const std::string &majorActor, const Date &date);
+
+  /**
+   * @brief
+   * 
+   * @param 
+   * 
+   * @return String reference to major actor
+   */
+  const std::string &getMajorActor() const;
+
+  /**
+   * @brief
+   * 
+   * @param
+   * 
+   * @return
+   */
+  bool operator<(const Classics &rhs) const;
+
+  /**
+   * @brief
+   * 
+   * @param
+   * 
+   * @return
+   */
+  bool operator==(const Classics &rhs) const;
+
+  /**
+   * @brief
+   * 
+   * @param
+   * 
+   * @return
+   */
+  int getHash() const;
 
 private:
   std::string majorActor;
