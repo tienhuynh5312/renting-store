5949a5c020aed4d2f67f7b7a3191872b4cefb1d1
erikj23 <erikj23.em@gmail.com>
2018-06-01 15:51:35 -0700

    comments

--- source/processtext.h
+++ source/processtext.h
@@ -15,13 +15,52 @@ enum class FileType
 class ProcessText
 {
 public:
+
+  /**
+   * @brief Construct a new Process Text object
+   */
   ProcessText(){};
+
+  /**
+   * @brief Destroy the Process Text object
+   */
   virtual ~ProcessText(){};
+
+  /**
+   * @brief 
+   * 
+   * @param fileType 
+   * @param path 
+   */
   void readFile(const FileType& fileType, const std::string &path);
 
 protected:
+
+  /**
+   * @brief 
+   * 
+   * @param command 
+   * @return true 
+   * @return false 
+   */
   virtual bool readTransaction(const std::string &command) = 0;
+
+  /**
+   * @brief 
+   * 
+   * @param command 
+   * @return true 
+   * @return false 
+   */
   virtual bool readCustomer(const std::string &command) = 0;
+  
+  /**
+   * @brief 
+   * 
+   * @param command 
+   * @return true 
+   * @return false 
+   */
   virtual bool readVideo(const std::string &command) = 0;
 };
 #endif // !_PROCESSTEXT_H
