dcd0d77274fb0cee0181c361bb64ab6726cfa799
BK1147 <32079725+BK1147@users.noreply.github.com>
2018-06-01 13:56:49 -0700

    date

--- source/date.h
+++ source/date.h
@@ -11,19 +11,76 @@ private:
   int day;
 
 public:
+  /**
+   * @brief Construct a new Date object
+   * 
+   */
   Date();
+  /**
+   * @brief Destroy the Date object
+   * 
+   */
   virtual ~Date();
+  /**
+   * @brief Construct a new Date object
+   * 
+   * @param date 
+   */
   Date(const Date &date);
+  /**
+   * @brief Construct a new Date object
+   * 
+   * @param y 
+   * @param m 
+   * @param d 
+   */
   Date(const int &y, const int &m = 1, const int &d = 1);
 
+  /**
+   * @brief Get the Year object
+   * 
+   * @return int 
+   */
   int getYear() const;
+  /**
+   * @brief Get the Month object
+   * 
+   * @return int 
+   */
   int getMonth() const;
+  /**
+   * @brief Get the Day object
+   * 
+   * @return int 
+   */
   int getDay() const;
 
+  /**
+   * @brief Set the Year object
+   * 
+   * @param year 
+   */
   void setYear(int year);
+  /**
+   * @brief Set the Month object
+   * 
+   * @param month 
+   */
   void setMonth(int month);
+  /**
+   * @brief Set the Day object
+   * 
+   * @param day 
+   */
   void setDay(int day);
 
+  /**
+   * @brief Arithematic operation to compare dates
+   * 
+   * @param rhs 
+   * @return true 
+   * @return false 
+   */
   bool operator<(const Date &rhs) const;
   bool operator>(const Date &rhs) const;
   bool operator==(const Date &rhs) const;
