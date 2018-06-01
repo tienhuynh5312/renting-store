3272c6bfc5882f605167fb41950ddcf792bd3634
erikj23 <erikj23.em@gmail.com>
2018-06-01 14:09:55 -0700

    comments

--- source/drama.h
+++ source/drama.h
@@ -13,48 +13,58 @@ class Drama : public Video
 public:
   /**
    * @brief Construct a new Drama object
-   * 
    */
   Drama() { videoType = VideoType::DRAMA; };
+  
   /**
    * @brief Destroy the Drama object
-   * 
    */
   virtual ~Drama();
 
   /**
    * @brief Construct a new Drama object
    * 
-   * @param title 
-   * @param stock 
-   * @param director 
+   * @param title title of video
+   * @param stock initial stock of video
+   * @param director director of video
    */
   Drama(const std::string &title, int stock, const std::string &director);
+  
   /**
    * @brief Construct a new Drama object
    * 
-   * @param title 
-   * @param stock 
-   * @param director 
-   * @param date 
+   * @param title title of video
+   * @param stock initial stock of video
+   * @param director director of video
+   * @param date date of video
    */
   Drama(const std::string &title, int stock, const std::string &director, const Date &date);
 
   /**
    * @brief Arithematic operation
    * 
-   * @param rhs 
-   * @return true 
-   * @return false 
+   * @param rhs object to compare this to
+   * @return true if this is less than argument
+   * @return false if this is greater than or equal to this object
    */
   bool operator<(const Drama &rhs) const;
+
+  /**
+   * @brief Arithematic operation
+   * 
+   * @param rhs object to compare this to
+   * @return true if this is equal to argument object
+   * @return false if this is not equal to this object
+   */
   bool operator==(const Drama &rhs) const;
+  
   /**
    * @brief Get the Hash object
    * 
-   * @return int 
+   * @return int that represents hash for this object
    */
   int getHash() const;
+  
   /**
    * @brief output stream
    * 
